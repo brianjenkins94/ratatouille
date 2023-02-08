10ee6842609fb318cc681771dfd22ef1c53deb36
Brian Jenkins <brianjenkins94@users.noreply.github.com>
2023-02-08 15:25:40 +0000

    Automated build

---
+++ docs/_next/static/t-CgS-BSyQQis21RgOP59/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bff950a31bfd5d70.css","static/chunks/pages/index-33f04c31a38c9197.js"],"/_error":["static/chunks/pages/_error-15a0bbaa3c2350f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
